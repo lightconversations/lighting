e9504a7d9970e62934c9e8eca6ea393f228d7161
lightconversations <tlaloc@lightconversations.com>
2017-10-03 13:54:19 -0500

    Updated the readme
    Added a list of column headers needed for using the output ASCII file with the EOS family consoles.

--- LightconversationsCues.py
+++ LightconversationsCues.py
@@ -150,6 +150,18 @@ This file converts a Comma separated value spreadsheet of CUES into a text
 file that can then be imported to a lighting desk. The original code was created by David Orlando.
 Tlaloc Lopez-Watermann added file name choice, export file name choice and cue list select.
 
+Please use the following label headers for you CSV file:
+CUE for the cue number
+UT for the Up Time
+DT for the Down Time
+Label for the cue label
+F/H for follow hang
+B for block cues. Must be a capital B to work
+PT for part
+Note for the notes
+Scene for scenes
+'please know that scenes are associated with cues and should not have their own row'. 
+
 ONLY USE ASCII IMPORT WITH NEW SHOWFILES
 ASCII import empites all the data of the current file and repopulates it ONLY with the data in the .txt file
 
