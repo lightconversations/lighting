844f5bbbcb451e11a938cd20add873b096c14a76
Light conversations LLC <tlaloc@lightconversations.com>
2018-10-23 15:18:48 -0400

    Features addition
    I am adding the ability to input the file locations for read and write.

--- LightconversationsCues.py
+++ LightconversationsCues.py
@@ -4,15 +4,25 @@ file that can then be imported to a lighting desk. The original code was created
 Tlaloc Lopez-Watermann added file name choice, export file name choice and cue list select.
 """
 
+
+
+
 name = raw_input("What's the csv filename that you want to convert to Eos_ASCII?--this should be a .csv file-- ")
+path = raw_input("Where is the file?--If no path is entered then file needs to be in folder containing program-- ")
 nameout = raw_input("What should your new filename be?--This should be a .txt file-- ")
+outpath = raw_input("Where do you want the new file to save?--If no path is entered then file will save in folder containing program--")
 cuelist = raw_input("Which cue list would you like to add this to? ")
 
+if path != '':
+    CSV=open(path+"/"+name).readlines()
+else:
+    CSV=open(name).readlines()
 
-CSV=open(name).readlines()
- 
+if path != '':
+    patch=open(outpath+"/"+nameout,'w') #Export file
+else:
+    patch=open(nameout,'w')
 
-patch=open(nameout,'w') #Export file
 CUE=['Ident 3:0',"\n",'Clear Cues', "\n",'Manufacturer ETC',"\n","Console Eos","\n",'$CueList'+' '+cuelist, "\n"]
 
 #print(CSV[0])
@@ -31,8 +41,8 @@ note_index=titles.index("Note")
 scene_index=titles.index("Scene")
 
 CSV.remove(CSV[0])
- 
- 
+
+
 
 for x in CSV:
     line=x.split(',',)
@@ -45,7 +55,7 @@ for x in CSV:
     q_num+=" 1"
     CUE.append(q_num)
     CUE.append("\n")
-    
+
     #PT
     if line[pt_index] != '':
         if line[pt_index] != '1':
@@ -55,7 +65,7 @@ for x in CSV:
         q_pt+=line[pt_index]
         CUE.append(q_pt)
         CUE.append("\n")
-    
+
     #UT
     if line[ut_index] != '':
         q_ut="Up "
@@ -67,8 +77,8 @@ for x in CSV:
         q_ut+=" 0 0 1"
         CUE.append(q_ut)
         CUE.append("\n")
-     
-    
+
+
     #DT
     if line[dt_index]!= '':
         q_dt="Down "
@@ -80,11 +90,11 @@ for x in CSV:
         q_dt+=" 0 0 1"
         CUE.append(q_dt)
         CUE.append("\n")
-    
+
     #chans
 #    CUE.append("Chan 999 0")
 #    CUE.append("\n")
-    
+
     #f
     if line[f_index] != '':
         f_h=line[f_index]
@@ -98,35 +108,35 @@ for x in CSV:
             q_f+=f_h[1:]
             CUE.append(q_f)
             CUE.append("\n")
-    
+
     #text
     if line[text_index] != '':
         text_dt="Text "
         text_dt+=line[text_index]
         CUE.append(text_dt)
         CUE.append("\n")
-    
+
     #Block
     if line[b_index] == 'B':
         q_b='$$Block'
         CUE.append(q_b)
         CUE.append("\n")
-    
+
     #Note
     if line[note_index] != '':
         text_text='$$CueNotes '
         text_text+=line[note_index]
         CUE.append(text_text)
         CUE.append("\n")
-    
+
     #scene
     if line[scene_index] != '':
         scene_text='$$scenetext '
         scene_text+=line[scene_index]
         CUE.append(scene_text)
         CUE.append("\n")
- 
-    
+
+
     CUE.append("\n")
 #
 CUE.append("EndData")
@@ -135,13 +145,13 @@ for x in CUE:
     patch.write(x)       #writes group to file
     print(x)
 patch.close()
- 
- 
- 
- 
- 
- 
- 
+
+
+
+
+
+
+
 
 #README FILE
 README=open('README.txt','w')
@@ -160,21 +170,16 @@ B for block cues. Must be a capital B to work
 PT for part
 Note for the notes
 Scene for scenes
-'please know that scenes are associated with cues and should not have their own row'. 
+'please know that scenes are associated with cues and should not have their own row'.
 
 !!!!!!!!!!ONLY USE ASCII IMPORT WITH NEW SHOWFILES!!!!!!!!!!!!!!!!
-
-ASCII import empties all the data of the current file and repopulates it ONLY with the data in the .txt file
-
+ASCII import empties all the data of the current file and repopulates it ONLY with the data in the .txt file.
+A good practice is to use this is to import the ASCII to new (merging) show file in an offline editor, then MERGE the cues to a show on the
+console. When merging make sure to MERGE ONLY THE CUES. The reason for doing it this way is so the patch at the theatre is not lost.
 !!!!!!!!!!ONLY USE ASCII IMPORT WITH NEW SHOWFILES!!!!!!!!!!!!!!!!
 
-
-the only information contained here is group information
-
-in EOS, this import will also create presets with identical information as the groups with all channels at 100%
 
 in EOS to import:
 -File>Import>USITT ASCII
 """)
 README.close()
-     
