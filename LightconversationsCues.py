315b29feb6ab0cdfd3f85996ae942de06cfd44e2
Light Conversations <tlaloc@lightconversations.com>
2021-11-07 02:21:08 -0500

    Commit

--- LightconversationsCues.py
+++ LightconversationsCues.py
@@ -5,40 +5,38 @@ Tlaloc Lopez-Watermann added file name choice, export file name choice and cue l
 """
 from pathlib import Path
 
-name = raw_input("What's the csv filename that you want to convert to Eos_ASCII?--this should be a .csv file-- ")
-path = Path(raw_input("Where is the file?--If no path is entered then file needs to be in folder containing program-- "))
-nameout = raw_input("What should your new filename be?--This should be a .txt file-- ")
-outpath = Path(raw_input("Where do you want the new file to save?--If no path is entered then file will save in folder containing program--"))
-cuelist = raw_input("Which cue list would you like to add this to? ")
+NAME = input("What's the csv filename that you want to convert Eos?")
+PATH = Path(input("Where is the file?--If no path is entered then file needs to be in folder containing program-- "))
+NAMEOUT = input("What should your new filename be? ")
+OUTPATH = Path(input("Where do you want the new file to save?"))
+CUELIST = input("Which cue list would you like to add this to? ")
 
-if path != '':
-    CSV=open(str(path / name)).readlines()
+if PATH != '':
+    CSV=open(str(PATH / NAME)).readlines()
 else:
-    CSV=open(name).readlines()
+    CSV=open(NAME).readlines()
 
-if outpath != '':
-    patch=open(str(outpath / nameout),'w') #Export file
+if OUTPATH != '':
+    PATCH=open(str(OUTPATH / NAMEOUT),'w') #Export file
 else:
-    patch=open(nameout,'w')
+    PATCH=open(NAMEOUT,'w')
 
 
 
-CUE=['Ident 3:0',"\n",'Clear Cues', "\n",'Manufacturer ETC',"\n","Console Eos","\n",'$CueList'+' '+cuelist,"\n"]
-     
+CUE=['Ident 3:0',"\n",'Clear Cues', "\n",'Manufacturer ETC',"\n","Console Eos","\n",'$CueList'+' '+CUELIST,"\n"]
 #print(CSV[0])
 
-titles=CSV[0]
-titles=titles.split(',')
-
-cue_index=titles.index("CUE")
-ut_index=titles.index("UT")
-dt_index=titles.index("DT")
-text_index=titles.index("Label")
-f_index=titles.index("F/H")
-b_index=titles.index("B")
-pt_index=titles.index("PT")
-note_index=titles.index("Note")
-scene_index=titles.index("Scene")
+TITLES=CSV[0]
+TITLES=TITLES.split(',')
+CUE_INDEX=TITLES.index("CUE")
+UT_INDEX=TITLES.index("UT")
+DT_INDEX=TITLES.index("DT")
+TEXT_INDEX=TITLES.index("Label")
+F_INDEX=TITLES.index("F/H")
+B_INDEX=TITLES.index("B")
+PT_INDEX=TITLES.index("PT")
+NOTE_INDEX=TITLES.index("Note")
+SCENE_INDEX=TITLES.index("Scene")
 
 CSV.remove(CSV[0])
 
@@ -48,45 +46,45 @@ for x in CSV:
     line=x.split(',',)
     #print(line)
     #Q number
-    if ">" in line[cue_index]:
-        line[cue_index]=line[cue_index][2:]
+    if ">" in line[CUE_INDEX]:
+        line[CUE_INDEX]=line[CUE_INDEX][2:]
     q_num="Cue "
-    q_num+=line[cue_index]
+    q_num+=line[CUE_INDEX]
     q_num+=" 1"
     CUE.append(q_num)
     CUE.append("\n")
 
     #PT
-    if line[pt_index] != '':
-        if line[pt_index] != '1':
-            print("i lie:",line[pt_index])
+    if line[PT_INDEX] != '':
+        if line[PT_INDEX] != '1':
+            print("i lie:",line[PT_INDEX])
             del CUE[CUE.index(q_num,len(CUE)-2)]
         q_pt="Part "
-        q_pt+=line[pt_index]
+        q_pt+=line[PT_INDEX]
         CUE.append(q_pt)
         CUE.append("\n")
 
     #UT
-    if line[ut_index] != '':
+    if line[UT_INDEX] != '':
         q_ut="Up "
-        q_ut+=line[ut_index]
+        q_ut+=line[UT_INDEX]
         CUE.append(q_ut)
         CUE.append("\n")
         q_ut="$$TimeUp "
-        q_ut+=line[ut_index]
+        q_ut+=line[UT_INDEX]
         q_ut+=" 0 0 1"
         CUE.append(q_ut)
         CUE.append("\n")
 
 
     #DT
-    if line[dt_index]!= '':
+    if line[DT_INDEX]!= '':
         q_dt="Down "
-        q_dt+=line[dt_index]
+        q_dt+=line[DT_INDEX]
         CUE.append(q_dt)
         CUE.append("\n")
         q_dt="$$TimeDown "
-        q_dt+=line[dt_index]
+        q_dt+=line[DT_INDEX]
         q_dt+=" 0 0 1"
         CUE.append(q_dt)
         CUE.append("\n")
@@ -96,8 +94,8 @@ for x in CSV:
 #    CUE.append("\n")
 
     #f
-    if line[f_index] != '':
-        f_h=line[f_index]
+    if line[F_INDEX] != '':
+        f_h=line[F_INDEX]
         if f_h[0] == 'F':
             q_f="Followon "
             q_f+=f_h[1:]
@@ -110,33 +108,33 @@ for x in CSV:
             CUE.append("\n")
 
     #text
-    if line[text_index] != '':
+    if line[TEXT_INDEX] != '':
         text_dt="Text "
-        text_dt+=line[text_index]
+        text_dt+=line[TEXT_INDEX]
         CUE.append(text_dt)
         CUE.append("\n")
 
     #Block
-    if line[b_index] == 'B':
+    if line[B_INDEX] == 'B':
         q_b='$$Block'
         CUE.append(q_b)
         CUE.append("\n")
-    if line[b_index] == 'I':
+    if line[B_INDEX] == 'I':
         q_b='$$IntBlock'
         CUE.append(q_b)
         CUE.append("\n")
 
     #Note
-    if line[note_index] != '':
+    if line[NOTE_INDEX] != '':
         text_text='$$CueNotes '
-        text_text+=line[note_index]
+        text_text+=line[NOTE_INDEX]
         CUE.append(text_text)
         CUE.append("\n")
 
     #scene
-    if line[scene_index] != '':
+    if line[SCENE_INDEX] != '':
         scene_text='$$scenetext '
-        scene_text+=line[scene_index]
+        scene_text+=line[SCENE_INDEX]
         CUE.append(scene_text)
         CUE.append("\n")
 
@@ -146,16 +144,9 @@ for x in CSV:
 CUE.append("EndData")
 #
 for x in CUE:
-    patch.write(x)       #writes group to file
+    PATCH.write(x)       #writes group to file
     print(x)
-patch.close()
-
-
-
-
-
-
-
+PATCH.close()
 
 #README FILE
 README=open('README.txt','w')
@@ -191,7 +182,7 @@ in EOS to import:
 README.close()
 
 
-#!!!!EOS ASCII COMMANDS FOR REFERENCE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#!!!!EOS ASCII COMMANDS FOR REFERENCE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #! Cues
 #! $CueList n      All subsequent cues are in cuelist n
 #!                 Cue lists can have a TEXT secondary and the following flags:
@@ -206,14 +197,12 @@ README.close()
 #! $$Stomp         see comment
 #! $$Exclude       see comment
 #! $$Control       number, fader mode, min, max, buttons 1-3 id/arg
-#! 
 #! $Cue:           All cues not in the first cue list and those
 #!                 with more than one digit after the decimal point
 #! $$Text          Cue/Part label
 #! $$Assert        Assert
 #! $$Block         Block
-#! $$IntBlock      IntBlock
-#! 
+#! $$IntBlock      IntBlocK
 #! The following cue times use this format:  Fade time, Delay time,
 #!                 Use default fadetime (1=true, 0=false, M=manual),
 #!                 Use default delay time (1=true, 0=false)
@@ -222,7 +211,6 @@ README.close()
 #! $$TimePosition  Focus times
 #! $$TimeColor     Color times
 #! $$TimeGraphic   Beam times
-#! 
 #! $$Rate          Rate (100 = normal)
 #! $$Follow        Auto-follow time counting from previous cue start
 #! $$Hang          Auto-follow time counting from previous cue completion
@@ -234,6 +222,6 @@ README.close()
 #! $$CueNotes      Cue Notes
 #! $$SceneText     Scene Text
 #! $$SceneEnd      Scene End flag
-#! 
+#!
 #! $$ChanMove      Intensity moves
 #! Chan            Tracked levels
